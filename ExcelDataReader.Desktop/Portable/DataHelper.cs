cfe1a2e8a0b039a4d96c92e40c993305860cc4ea
Mj Ebrahim <mj.taruo@gmail.com>
2016-11-22 17:56:57 +0300

    Added

---
+++ ExcelDataReader.Desktop/Portable/DataHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ExcelDataReader.Portable.Data;
+
+namespace ExcelDataReader.Desktop.Portable
+{
+    public class DataHelper : IDataHelper
+    {
+        public bool IsDBNull(object value)
+        {
+            return Convert.IsDBNull(value);
+        }
+    }
+}
